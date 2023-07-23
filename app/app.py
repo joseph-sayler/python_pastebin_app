02553e6ab58a62361e258db1bff45bd4dc332832
joseph-sayler <joseph.sayler@gmail.com>
2023-07-22 18:48:49 -0700

    add new routes

--- app/app.py
+++ app/app.py
@@ -1,13 +1,15 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from app.routers.pastes import create, read
+from app.routers.pastes import create, delete, read, update
 from config import Config
 
 app = FastAPI()
 
 app.include_router(read.router)
 app.include_router(create.router)
+app.include_router(delete.router)
+app.include_router(update.router)
 
 app.add_middleware(
     CORSMiddleware,
